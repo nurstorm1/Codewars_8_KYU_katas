9d7b953f989c8767b7af1a2214cad515ee3ce7ba
Nuridin Aiupdzhanov <114620322+nurstorm1@users.noreply.github.com>
2023-04-21 20:55:46 +0000

    push

--- 8kyu tasks/Cat years, Dog years.js
+++ 8kyu tasks/Cat years, Dog years.js
@@ -15,7 +15,29 @@ var humanYearsCatYearsDogYears = function(humanYears) {
 
 
 // Your code here!
-/* const cat = 1; 
+/* 
+
+Kata Task
+I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+
+const cat = 1; 
       const dog = 1; 
       if(humanYears === 1) {
         cat * 15;
