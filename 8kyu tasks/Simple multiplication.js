852f56a165c9154aeb71fc0d48bf44d5fe5be884
Nuridin Aiupdzhanov <114620322+nurstorm1@users.noreply.github.com>
2023-04-22 21:43:44 +0000

    push

---
+++ 8kyu tasks/Simple multiplication.js
@@ -0,0 +1,9 @@
+function simpleMultiplication(number) {
+    // your code........
+    if (number % 2 === 0) {
+        return (number * 8);
+    }
+    if (number % 2 !== 0) {
+        return (number * 9);
+    }
+}
